397e3e60ae2f81b09fdac112e84a7cadab713922
Jarod <4776246@qq.com>
2020-05-31 14:13:09 +0800

    SSM整合框架代码

---
+++ ssmbuild_ZH/src/main/java/com/jarod/dao/BookMapper.java
@@ -0,0 +1,25 @@
+package com.jarod.dao;
+
+import com.jarod.pojo.Books;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+public interface BookMapper {
+
+    //增加一门书
+    int addBook(Books books);
+
+    //删除一门书
+    int deleteBookById(@Param("bookId") int id);
+
+    //更新一本书
+    int updateBook(Books books);
+
+    //查询一本书
+    Books queryBookById(@Param("bookId") int id);
+
+    //查询全部的书
+    List<Books> queryAllBook();
+
+}
---
+++ ssmbuild_ZH/src/main/java/com/jarod/service/BookService.java
@@ -0,0 +1,25 @@
+package com.jarod.service;
+
+import com.jarod.pojo.Books;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+public interface BookService {
+
+    //增加一门书
+    int addBook(Books books);
+
+    //删除一门书
+    int deleteBookById(int id);
+
+    //更新一本书
+    int updateBook(Books books);
+
+    //查询一本书
+    Books queryBookById(int id);
+
+    //查询全部的书
+    List<Books> queryAllBook();
+
+}
